48eda80bca63409b100db2889388a73f1e309c9a
nohandev <nohanpablo759@gmail.com>
2024-12-19 08:05:27 -0300

    Final considerations added in Footer component

---
+++ src/assets/cardsBanner.js
@@ -0,0 +1,15 @@
+import card1 from './cards_banner/Payment Methods 1.png';
+import card2 from './cards_banner/Payment Methods 2.png';
+import card3 from './cards_banner/Payment Methods 3.png';
+import card4 from './cards_banner/Payment Methods 4.png';
+import card5 from './cards_banner/Payment Methods 5.png';
+import card6 from './cards_banner/Payment Methods 6.png';
+
+export default {
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebook, faYoutube  } from '@fortawesome/free-brands-svg-icons';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
+import cards from '../assets/cardsBanner.js';
 
 const Footer = () => {
 
-  const [ email, setEmail] = useState('')
+  const [email, setEmail] = useState('')
+
+  const captureEmail = () => {
+    if (email === '') {
+      alert("O campo do email está vazio!")
+    } else {
+    console.log(email)
+    setEmail('')
+    }
+  }
+
+  const paymentsMethods = [
+    {id: 1, card: cards.card1, alt: 'card banner'},
+    {id: 2, card: cards.card2, alt: 'card banner'},
+    {id: 3, card: cards.card3, alt: 'card banner'},
+    {id: 4, card: cards.card4, alt: 'card banner'},
+    {id: 5, card: cards.card5, alt: 'card banner'},
+    {id: 6, card: cards.card6, alt: 'card banner'}
+  ]
 
   return (
     <footer className={style.footerContainer}>
@@ -34,8 +53,9 @@ const Footer = () => {
           </div>
           <div className={style.description}>
             <p>Home</p>
-            <p>Shope</p>
+            <p>Shop</p>
             <p>Product</p>
+            <p>Articles</p>
             <p>Contact Us</p>
           </div>
         </div>
@@ -58,9 +78,34 @@ const Footer = () => {
             <p>Subscribe our newsletter to get more deals, new products and promotions</p>
           </div>
           <div className={style.inputContainer}>
-            <input type="text" placeholder='Enter your email' />
-            <button><FontAwesomeIcon icon={faArrowRight} /></button>
+            <input
+            type="text" 
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+             />
+            <button 
+            className={style.submitEmail}
+            type='submit'
+            onClick={captureEmail}
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
           </div>
+        </div>
+      </section>
+      <section className={style.finalConsiderations}>
+        <div className={style.privacyPolicyContainer}>
+          <p>Copyright © 2023 3legant. All rights reserved</p>
+          <span>|</span>
+          <p>Privacy Policy</p>
+          <p>Terms & Conditions</p>
+        </div>
+
+        <div className={style.cardsLogosContainer}>
+          {paymentsMethods.map(card => (
+            <img key={card.id} src={card.card} alt={card.alt}/>
+          ))}
         </div>
       </section>
     </footer>
